7030c3fedffd52beb46282b9f9a3ccb4ccc16bf2
l0o0 <>
2020-06-21 22:48:18 +0800

    A simple demo for search and retrive cnki meta data.

--- chrome/content/scripts/jasminum.js
+++ chrome/content/scripts/jasminum.js
@@ -1,20 +1,21 @@
 Zotero.Jasminum = {
   init: function () {
-    // Register the callback in Zotero as an item observer
-    var notifierID = Zotero.Notifier.registerObserver(
-      Zotero.Jasminum.notifierCallback,
-      ["item"]
-    );
+    // // Register the callback in Zotero as an item observer
+    // var notifierID = Zotero.Notifier.registerObserver(
+    //   Zotero.Jasminum.notifierCallback,
+    //   ["item"]
+    // );
 
-    // Unregister callback when the window closes (important to avoid a memory leak)
-    window.addEventListener(
-      "unload",
-      function (e) {
-        Zotero.Notifier.unregisterObserver(notifierID);
-      },
-      false
-    );
+    // // Unregister callback when the window closes (important to avoid a memory leak)
+    // window.addEventListener(
+    //   "unload",
+    //   function (e) {
+    //     Zotero.Notifier.unregisterObserver(notifierID);
+    //   },
+    //   false
+    // );
   },
+  
   notifierCallback: {
     // Check new added item, and adds meta data.
     // TODO Add a check function here
@@ -33,7 +34,7 @@ Zotero.Jasminum = {
     var show_menu = items.some(item => Zotero.Jasminum.checkItem(item));
     pane.document.getElementById("zotero-itemmenu-jasminum").hidden = !show_menu;
     pane.document.getElementById("id-jasminum-separator").hidden = !show_menu;
-    console.log(show_menu);
+    // console.log(show_menu);
   },
 
   updateSelectedEntity: function (libraryId) {
@@ -55,6 +56,7 @@ Zotero.Jasminum = {
       Zotero.Jasminum.updateItems(items, suppress_warnings);
     }
   },
+
   updateSelectedItems: function () {
     Zotero.debug("Updating Selected items");
     suppress_warnings = false;
@@ -63,32 +65,6 @@ Zotero.Jasminum = {
       suppress_warnings
     );
   },
-//   updateAll: function () {
-//     Zotero.debug("Updating all items in Zotero");
-//     var items = [];
-
-//     // Get all items
-//     Zotero.Items.getAll().then(function (items) {
-//       // Once we have all items, make sure it's a regular item.
-//       // And that the library is editable
-//       // Then add that item to our list.
-//       items.map(function (item) {
-//         if (item.isRegularItem() && !item.isCollection()) {
-//           var libraryId = item.getField("libraryID");
-//           if (
-//             libraryId == null ||
-//             libraryId == "" ||
-//             Zotero.Libraries.isEditable(libraryId)
-//           ) {
-//             items.push(item);
-//           }
-//         }
-//       });
-//     });
-//     // Update all of our items with pdfs.
-//     suppress_warnings = true;
-//     Zotero.Jasminum.updateItems(items, suppress_warnings);
-//   },
 
   checkItem: function (item) {
     // filename like author_title.ext, ext=pdf/caj
@@ -104,8 +80,8 @@ Zotero.Jasminum = {
     var prefix = filename.substr(0, filename.length - 4);
     var prefixArr = prefix.split("_");
     return {
-      author: prefixArr[0],
-      keyword: prefixArr[1],
+      author: prefixArr[prefixArr.length-1],
+      keyword: prefixArr.slice(0, prefixArr.length-1).join(' '),
     };
   },
 
@@ -147,6 +123,7 @@ Zotero.Jasminum = {
   },
 
   selectRow: function (rowSelectors) {
+    console.log('select window start');
     var io = { dataIn: rowSelectors, dataOut: null };
     var newDialog = window.openDialog(
       "chrome://zotero/content/ingester/selectitems.xul",
@@ -182,6 +159,10 @@ Zotero.Jasminum = {
       "&CookieName=FileNameS";
   },
 
+  cleanItem: function () {
+    console.log('clean parent');
+  },
+
   updateItems: function (items, suppress_warnings) {
     if (items.length == 0) return;
     var item = items.shift();
@@ -212,15 +193,26 @@ Zotero.Jasminum = {
         // //table[@class='GridTableContent']//tr[not (@class)]
         var rows = html.querySelectorAll("table.GridTableContent > tbody > tr");
         console.log(rows.length);
-        // Get the right item from search result.
-        var rowIndicators = {};
-        for (let idx = 1; idx < rows.length; idx++) {
-          var rowText = rows[idx].textContent.split(/\s+/).join(" ");
-          rowIndicators[rowText] = idx;
-          console.log(rowText);
+        // return null when no items in result
+        var targetRow;
+        if (rows.length <= 1) {
+          console.log('No items found.');
+          return 
+        } else if (rows.length = 2) {
+          targetRow = rows[1];
+        } else {
+          // Get the right item from search result.
+          var rowIndicators = {};
+          for (let idx = 1; idx < rows.length; idx++) {
+            var rowText = rows[idx].textContent.split(/\s+/).join(" ");
+            rowIndicators[rowText] = idx;
+            console.log(rowText);
+          }
+          var targetIndicator = Zotero.Jasminum.selectRow(rowIndicators);
+          targetRow = rows[Object.values(targetIndicator)[0]];
         }
-        var targetIndicator = Zotero.Jasminum.selectRow(rowIndicators);
-        var targetRow = rows[Object.values(targetIndicator)[0]];
+        console.log(targetRow.textContent);
+        
         // Retrive selected item meta data.
         var targetUrl = targetRow.getElementsByClassName("fz14")[0].href;
         var targetID = Zotero.Jasminum.getIDFromUrl(targetUrl);
@@ -228,9 +220,9 @@ Zotero.Jasminum = {
         // Get reference data from CNKI by ID.
         var httpRequest = new XMLHttpRequest();
         var postData = "formfilenames=" + encodeURIComponent(targetID.dbname + "!" + targetID.filename + "!1!0,")
-		    + '&hid_kLogin_headerUrl=/KLogin/Request/GetKHeader.ashx%3Fcallback%3D%3F'
-		    + '&hid_KLogin_FooterUrl=/KLogin/Request/GetKHeader.ashx%3Fcallback%3D%3F'
-		    + '&CookieName=FileNameS';
+		      + '&hid_kLogin_headerUrl=/KLogin/Request/GetKHeader.ashx%3Fcallback%3D%3F'
+		      + '&hid_KLogin_FooterUrl=/KLogin/Request/GetKHeader.ashx%3Fcallback%3D%3F'
+		      + '&CookieName=FileNameS';
         httpRequest.open(
           "GET",
           "https://kns.cnki.net/kns/ViewPage/viewsave.aspx?displayMode=Refworks&" +
@@ -264,6 +256,51 @@ Zotero.Jasminum = {
                 return tag + " " + authors.join("\n" + tag + " ");
               });
             console.log(data);
+            var translate = new Zotero.Translate.Import();
+            translate.setTranslator("1a3506da-a303-4b0a-a1cd-f216e6138d86");
+            translate.setString(data);
+            translate.setHandler('itemDone', function (obj, newItem) {
+              item.setSource(newItem._id);
+              item.save();
+              console.log(newItem);
+              // split names
+              for (var i = 0, n = newItem._creators.length; i < n; i++) {
+                var creator = newItem._creators[i];
+                if (creator.firstName) continue;
+                
+                var lastSpace = creator.lastName.lastIndexOf(' ');
+                if (creator.lastName.search(/[A-Za-z]/) !== -1 && lastSpace !== -1) {
+                  // western name. split on last space
+                  creator.firstName = creator.lastName.substr(0, lastSpace);
+                  creator.lastName = creator.lastName.substr(lastSpace + 1);
+                }
+                else {
+                  // Chinese name. first character is last name, the rest are first name
+                  creator.firstName = creator.lastName.substr(1);
+                  creator.lastName = creator.lastName.charAt(0);
+                }
+              }
+              // clean up tags. Remove numbers from end
+              for (var j = 0, l = newItem._tags.length; j < l; j++) {
+                newItem._tags[j] = newItem._tags[j].replace(/:\d+$/, '');
+              }
+
+              if (newItem._abstractNote) {
+                newItem.abstractNote = newItem._abstractNote.replace(/\s*[\r\n]\s*/g, '\n')
+                                      .replace(/&lt;.*?&gt;/g, "");
+              }
+              newItem._title = Zotero.Utilities.trimInternal(newItem._title);
+              // CN 中国刊物编号，非refworks中的callNumber
+              // CN in CNKI refworks format explains Chinese version of ISSN
+              if (newItem._callNumber) {
+              //	newItem.extra = 'CN ' + newItem.callNumber;
+                newItem._callNumber = "";
+              }
+              // newItem.complete();
+              console.log(newItem);
+            });
+            
+            translate.translate(item.libraryID, false);
           }
         };
       };
