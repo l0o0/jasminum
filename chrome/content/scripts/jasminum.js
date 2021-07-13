c56956e253b983900621f3dc369e6cb1f104a860
Linxzh <linxzh1989@gmail.com>
2021-07-13 17:00:19 +0800

    fix: 修复添加书签后PDF附件丢失问题 fixed #40

--- chrome/content/scripts/jasminum.js
+++ chrome/content/scripts/jasminum.js
@@ -187,50 +187,50 @@ Zotero.Jasminum = {
         // Make query parameters from filename
         var patent = Zotero.Prefs.get("jasminum.namepatent");
         var patentSepArr = patent.split(/{%[^}]+}/);
-        var patentSepRegArr = patentSepArr.map(x => x.replace(/([\[\\\^\$\.\|\?\*\+\(\)])/g,'\\$&'));
+        var patentSepRegArr = patentSepArr.map(x => x.replace(/([\[\\\^\$\.\|\?\*\+\(\)])/g, '\\$&'));
         var patentMainArr = patent.match(/{%[^}]+}/g);
         //文件名中的作者姓名字段里不能包含下划线，请使用“&,，”等字符分隔多个作者，或仅使用第一个作者名（加不加“等”都行）。
-        var patentMainRegArr = patentMainArr.map(x => x.replace(/.+/,/{%y}/.test(x)?'(\\d+)':(/{%a}/.test(x)?'([^_]+)':'(.+)')));
-        var regStrInterArr = patentSepRegArr.map((_,i)=>[patentSepRegArr[i],patentMainRegArr[i]]);
-        var patentReg = new RegExp([].concat.apply([],regStrInterArr).filter(Boolean).join(''),'g');
+        var patentMainRegArr = patentMainArr.map(x => x.replace(/.+/, /{%y}/.test(x) ? '(\\d+)' : (/{%a}/.test(x) ? '([^_]+)' : '(.+)')));
+        var regStrInterArr = patentSepRegArr.map((_, i) => [patentSepRegArr[i], patentMainRegArr[i]]);
+        var patentReg = new RegExp([].concat.apply([], regStrInterArr).filter(Boolean).join(''), 'g');
         var prefix = filename.substr(0, filename.length - 4);
         var prefix = prefix.replace(/\.ashx$/g, ""); // 删除末尾.ashx字符
         var prefixMainArr = patentReg.exec(prefix);
         var titleIdx = patentMainArr.indexOf('{%t}');
         var authorIdx = patentMainArr.indexOf('{%a}');
-        var titleRaw = (titleIdx!=-1)?prefixMainArr[titleIdx+1]:'';
-        var authors = (authorIdx!=-1)?prefixMainArr[authorIdx+1]:'';
+        var titleRaw = (titleIdx != -1) ? prefixMainArr[titleIdx + 1] : '';
+        var authors = (authorIdx != -1) ? prefixMainArr[authorIdx + 1] : '';
         var authorArr = authors.split(/[,，&]/);
         var author = authorArr[0]
-        if (authorArr.length == 1){
-          //删除名字后可能出现的“等”字，此处未能做到识别该字是否属于作者姓名。
-          //这种处理方式的问题：假如作者名最后一个字为“等”，例如：“刘等”，此时会造成误删。
-          //于是对字符数进行判断，保证删除“等”后，至少还剩两个字符，尽可能地避免误删。
-        
-          author = (author.endsWith('等')&&author.length>2)?author.substr(0, author.length - 1):author;
+        if (authorArr.length == 1) {
+            //删除名字后可能出现的“等”字，此处未能做到识别该字是否属于作者姓名。
+            //这种处理方式的问题：假如作者名最后一个字为“等”，例如：“刘等”，此时会造成误删。
+            //于是对字符数进行判断，保证删除“等”后，至少还剩两个字符，尽可能地避免误删。
+
+            author = (author.endsWith('等') && author.length > 2) ? author.substr(0, author.length - 1) : author;
         }
 
         //为了避免文件名中的标题字段里存在如下两种情况而导致的搜索失败:
-            //原标题过长，文件名出现“_省略_”；
-            //原标题有特殊符号（如希腊字母、上下标）导致的标题变动，此时标题也会出现“_”。
+        //原标题过长，文件名出现“_省略_”；
+        //原标题有特殊符号（如希腊字母、上下标）导致的标题变动，此时标题也会出现“_”。
         //于是只取用标题中用“_”分割之后的最长的部分作为用于搜索的标题。
-        
+
         //这种处理方式的问题：假如“最长的部分”中存在知网改写的部分，也可能搜索失败。
         //不过这只是理论上可能存在的情形，目前还未实际遇到。
-        
+
         var title;
-        
-        if (/_/.test(titleRaw)){
-          
-          //getLongestText函数，用于拿到字符串数组中的最长字符
-          //摘自https://stackoverflow.com/a/59935726
-          const getLongestText = (arr) => arr.reduce(
-            (savedText, text) => (text.length > savedText.length ? text : savedText),
-            '',
-          );
-          title = getLongestText(titleRaw.split(/_/));
-        }else{
-          title = titleRaw;
+
+        if (/_/.test(titleRaw)) {
+
+            //getLongestText函数，用于拿到字符串数组中的最长字符
+            //摘自https://stackoverflow.com/a/59935726
+            const getLongestText = (arr) => arr.reduce(
+                (savedText, text) => (text.length > savedText.length ? text : savedText),
+                '',
+            );
+            title = getLongestText(titleRaw.split(/_/));
+        } else {
+            title = titleRaw;
         }
 
         return {
@@ -755,14 +755,16 @@ Zotero.Jasminum = {
                 ) {
                     Zotero.ZotFile.renameSelectedAttachments();
                 }
+
+                await item.saveTx();
+                await newItem.saveTx();
+                // Add bookmark after attaching to  new item
                 if (
                     Zotero.Prefs.get("jasminum.autobookmark") &&
                     Zotero.Jasminum.checkItemPDF(item)
                 ) {
                     await Zotero.Jasminum.addBookmarkItem(item);
                 }
-                await item.saveTx();
-                await newItem.saveTx();
             } else {
                 // 有多个返回结果，将文件与新条目关联，用于用户后续手动选择
                 newItems.forEach(function (newItem) {
