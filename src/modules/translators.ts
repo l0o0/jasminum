793c4bdbaff85e1d18b1b9fc3337250d1fbb96c1
jiaojiaodubai <63148861+jiaojiaodubai@users.noreply.github.com>
2025-04-13 13:59:45 +0800

    Update translators.ts

--- src/modules/translators.ts
+++ src/modules/translators.ts
@@ -86,6 +86,7 @@ export async function updateTranslators(force = false): Promise<boolean> {
     return false;
   }
   updating = true;
+  await Zotero.Schema.schemaUpdatePromise;
   let needUpdate = false;
   const lastUpdateTime = getPref("translatorUpdateTime");
   const now = Date.now();
